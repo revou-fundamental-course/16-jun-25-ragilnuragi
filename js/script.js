1f2067751b323b8ab3c4bf7d0b790e0b17533f1e
ragilnuragi <ragil1398@gmail.com>
2025-06-24 20:13:44 +0700

    menghapus jajargenjang dan menambahkan validate serta mempercantik tampilan

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 function luasSegitiga() {
   const alas = document.getElementById("alas").value;
   const tinggi = document.getElementById("tinggi").value;
+  const errorLuas = document.getElementById("errorLuas");
 
   if (alas === "" || tinggi === "") {
-    alert("Mohon isi alas dan tinggi");
+    errorLuas.innerText = "Mohon isi semua nilai alas dan tinggi!";
   } else {
     const luas = (alas * tinggi) / 2;
     document.getElementById("rumusLuas").innerHTML = "L = (Alas * Tinggi) / 2";
     document.getElementById("nilaiLuas").innerHTML = `L = (${alas} * ${tinggi}) / 2`;
     document.getElementById("luasJawaban").innerHTML = luas;
+    errorLuas.innerText = "";
   }
 }
 
@@ -16,42 +18,16 @@ function kelilingSegitiga() {
   const sisiA = Number(document.getElementById("sisi-a").value);
   const sisiB = Number(document.getElementById("sisi-b").value);
   const sisiC = Number(document.getElementById("sisi-c").value);
+  const errorKeliling = document.getElementById("errorKeliling");
 
   if (sisiA == "" || sisiB == "" || sisiC == "") {
-    alert("Mohon isi sisinya");
+    errorKeliling.innerText = "Mohon isi semua sisi segitiga!";
   } else {
     const keliling = sisiA + sisiB + sisiC;
     document.getElementById("rumusKeliling").innerHTML = "K = S1 + S2 + S3";
     document.getElementById("nilaiKeliling").innerHTML = `K = ${sisiA} + ${sisiB} + ${sisiC}`;
     document.getElementById("kelilingJawaban").innerHTML = keliling;
-  }
-}
-
-function luasJajargenjang() {
-  const alas = document.getElementById("alasJajargenjang").value;
-  const tinggi = document.getElementById("tinggiJajargenjang").value;
-
-  if (alas === "" || tinggi === "") {
-    alert("Mohon isi alas dan tinggi");
-  } else {
-    const luas = alas * tinggi;
-    document.getElementById("rumusLuasJajargenjang").innerHTML = "L = Alas * Tinggi";
-    document.getElementById("nilaiLuasJajargenjang").innerHTML = `L = (${alas} * ${tinggi})`;
-    document.getElementById("luasJawabanJajargenjang").innerHTML = luas;
-  }
-}
-
-function kelilingJajargenjang() {
-  const alas = document.getElementById("panjang-alas").value;
-  const miring = document.getElementById("panjang-miring").value;
-
-  if (alas === "" || miring === "") {
-    alert("Mohon isi panjang alas dan miring");
-  } else {
-    const keliling = 2 * alas + 2 * miring;
-    document.getElementById("rumusKelilingJajargenjang").innerHTML = "K = 2 * Alas + 2 * Miring";
-    document.getElementById("nilaiKelilingJajargenjang").innerHTML = `K = (2 * ${alas}) + (2 * ${miring})`;
-    document.getElementById("kelilingJawabanJajargenjang").innerHTML = keliling;
+    errorKeliling.innerText = "";
   }
 }
 
@@ -62,17 +38,11 @@ function resetForm(formId) {
     document.getElementById("luasJawaban").innerHTML = "";
     document.getElementById("nilaiLuas").innerHTML = "";
     document.getElementById("rumusLuas").innerHTML = "";
+    document.getElementById("errorLuas").innerText = "";
   } else if (formId === "kelilingSegitiga") {
     document.getElementById("kelilingJawaban").innerHTML = "";
     document.getElementById("nilaiKeliling").innerHTML = "";
     document.getElementById("rumusKeliling").innerHTML = "";
-  } else if (formId === "luasJajargenjang") {
-    document.getElementById("luasJawabanJajargenjang").innerHTML = "";
-    document.getElementById("nilaiLuasJajargenjang").innerHTML = "";
-    document.getElementById("rumusLuasJajargenjang").innerHTML = "";
-  } else if (formId === "kelilingJajargenjang") {
-    document.getElementById("kelilingJawabanJajargenjang").innerHTML = "";
-    document.getElementById("nilaiKelilingJajargenjang").innerHTML = "";
-    document.getElementById("rumusKelilingJajargenjang").innerHTML = "";
+    document.getElementById("errorKeliling").innerText = "";
   }
 }
